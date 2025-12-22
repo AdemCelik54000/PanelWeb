65c46db0b008d3fadc260b4ab5fb3d20ee4ebb1a
AdemCelik54000 <adem.celik@epita.fr>
2025-12-22 21:18:51 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+    <div className="min-h-screen bg-gray-100">
+      <p
+        style={{
+          position: "absolute",
+          left: "-9999px",
+          top: "auto",
+          width: "1px",
+          height: "1px",
+          overflow: "hidden",
+        }}
+      >
+        Start prompting (or editing) to see magic happen :)
+      </p>
+      <iframe
+        title="Upload chantier"
+        src="/upload.html"
+        style={{
+          border: "none",
+          width: "100%",
+          height: "100vh",
+          display: "block",
+          background: "#0f172a",
+        }}
+      />
     </div>
   );
 }
